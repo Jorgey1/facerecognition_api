4b2868365c80ebefc41e26fe1003ed94905640a7
Jorgeyv <jorge.veronique@appointmentgroup.com>
2020-10-30 12:26:39 +0000

    updated routes

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 
 
 const app = express();
 app.use(bodyParser.json())
+app.use(cors())
 const database = {
     users: [
         {
             id: '123',
             name: 'john',
             email: 'john@gmail.com',
+            password: 'cookies',
             entries: 0,
             joined: new Date()
         },
@@ -19,6 +22,7 @@ const database = {
             id: '124',
             name: 'sally',
             email: 'sally@gmail.com',
+            password: 'bananas',
             entries: 0,
             joined: new Date()
         }
@@ -39,9 +43,9 @@ app.get('/', (req, res) => {
 app.post('/signin', (req, res) => {
     if( req.body.email === database.users[0].email && 
         req.body.password === database.users[0].password) {
-    res.json('success');
+    res.json(database.users[0]);
     } else {
-    res.status(400).json('error logging in')
+    res.status(400).json('error logging in');
     }
 })
 
@@ -51,7 +55,6 @@ app.post('/register', (req, res) => {
             id: '125',
             name: name,
             email: email,
-            password: password,
             entries: 0,
             joined: new Date()
     })
@@ -72,7 +75,7 @@ app.get('/profile/:id', (req, res) => {
     }
 })
 
-app.post('/image', (req, res) => {
+app.put('/image', (req, res) => {
     const { id } = req.body;
     let found = false
     database.users.forEach(user => {
